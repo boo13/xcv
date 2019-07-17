68d501d90abf170bc90ffad97a809ef1515abc4b
boo 13 <boo13bot@gmail.com>
2019-07-17 04:02:32 -0400

    move settings into main scipt

--- xcv/settings.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Settings:
-    timerFlag: int = 10
-    verbose: bool = False
-    serialPort: str = "/dev/cu.usbmodem58290301"
-    serialBaud: int = 9600
