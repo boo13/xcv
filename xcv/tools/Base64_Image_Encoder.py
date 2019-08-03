ba3263244641aff251861aa8af19573f41e5c5e7
boo 13 <boo13bot@gmail.com>
2019-08-03 19:27:43 -0400

    thanks to pysimplegui

---
+++ xcv/tools/Base64_Image_Encoder.py
@@ -0,0 +1,42 @@
+import PySimpleGUI as sg
+import os
+import base64
+
+'''
+    Base64 Encoder - encodes a folder of PNG files and creates a .py file with definitions
+
+    This is an unmodified version of the Demo Program from PySimpleGUI
+    https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Base64_Image_Encoder.py
+
+'''
+
+def main():
+    OUTPUT_FILENAME = 'base64_output.py'
+    folder=''
+    folder = sg.PopupGetFolder('Source folder for images\nImages will be encoded and results saved to the same folder',
+                               title='Base64 Encoder',
+                               default_path=folder, initial_folder=folder )
+
+    if folder is None or folder == '':
+        sg.PopupCancel('Cancelled - No valid folder entered')
+        return
+    try:
+        namesonly = [f for f in os.listdir(folder) if f.endswith('.png') or f.endswith('.ico')]
+    except:
+        sg.PopupCancel('Cancelled - No valid folder entered')
+        return
+
+    outfile = open(os.path.join(folder, OUTPUT_FILENAME), 'w')
+
+    for i, file in enumerate(namesonly):
+        contents = open(os.path.join(folder, file), 'rb').read()
+        encoded = base64.b64encode(contents)
+        outfile.write('\n{} = {}\n\n'.format(file[:file.index(".")], encoded))
+        sg.OneLineProgressMeter('Base64 Encoding', i+1, len(namesonly),key='_METER_')
+
+    outfile.close()
+    sg.Popup('Completed!', 'Encoded %s files'%(i+1))
+
+
+if __name__ == '__main__':
+    main()
