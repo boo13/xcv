54a2aae268e2f1423b7ff236d3b7f277df1ca5fa
boo 13 <boo13bot@gmail.com>
2019-08-06 22:32:41 -0400

    black

--- setup.py
+++ setup.py
@@ -5,45 +5,41 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ["Click>=6.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Randy Boo13 Boo",
-    author_email='boo13bot@gmail.com',
+    author_email="boo13bot@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
     description="xcv is a project that pushes buttons on a controller.",
-    entry_points={
-        'console_scripts': [
-            'xcv=xcv.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["xcv=xcv.cli:main_input"]},
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='xcv',
-    name='xcv',
-    packages=find_packages(include=['xcv']),
+    keywords="xcv",
+    name="xcv",
+    packages=find_packages(include=["xcv"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/boo13/xcv',
-    version='0.1.0',
+    url="https://github.com/boo13/xcv",
+    version="0.1.2",
     zip_safe=False,
 )
