773b8d71a14957daaeebf7c6e4c0b06ae5ad3d59
boo 13 <boo13bot@gmail.com>
2019-08-06 22:31:07 -0400

    fix import

--- xcv/__main__.py
+++ xcv/__main__.py
@@ -8,8 +8,7 @@ startTime = datetime.datetime.now(tz=pytz.UTC).astimezone(
     pytz.timezone(Settings.TIMEZONE)
 )
 
-from cli import cli
-
+import cli
 import sys
 
 sys.exit(cli.main_input())  # pragma: no cover
