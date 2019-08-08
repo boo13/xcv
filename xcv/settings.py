17c534db7efe739f85801583f8b67cffe2d4ca00
boo 13 <boo13bot@gmail.com>
2019-08-08 03:19:52 -0400

    refactor

--- xcv/settings.py
+++ xcv/settings.py
@@ -1,83 +1,95 @@
+# settings.py
+
+import sys
+from pathlib import Path
 from dataclasses import dataclass
 
+# class SerialError(Exception):
+#     pass
 
-# Settings - we use these to set (and mostly forget) the config
-@dataclass
-class Settings:
-    # __slots__ = [
-    #     "TIMEZONE",
-    #     "timer",
-    #     "verbose",
-    #     "PACKAGE_NAME",
-    #     "XCV_VERSION",
-    #     "XCV_AUTHOR",
-    #     "XCV_EMAIL",
-    #     "XCV_DESCRIPTION",
-    #     "SERIAL_BAUD",
-    #     "WINDOWS",
-    #     "SERIAL_PORT",
-    # ]
 
-    # ====================================
-    #   User Settings
-    # ====================================
-    TIMEZONE: str = "US/Eastern"
-    timer: int = 10
-    verbose: bool = False
+class xcv_api:
+    os_is_windows = False
+    if not os_is_windows:
+        emojis = {"boo": "👻", "robot": "🤖", "stars": "✨ ✨ ✨"}
+    path: Path = Path(__file__).resolve().parent
+    timezone = "US/Eastern"
+    timer = 10
+    verbose = False
+    # ...
+    debug_serial = False
+    # Game stuff
+    btn_utf_send_commands = ["A", "B", "X", "Y", "S", "l", "r", "w", "a", "s", "d", "o", "p"]
+    
 
-    # ====================================
-    #   Package Info
-    # ====================================    
-    XCV_DESCRIPTION: str = f"""
-        The project's goal is to make game-based OpenCV experiments easier.
+# > PEP8: The naming convention for functions may be used instead in cases where the interface is documented and used primarily as a callable.
+# QUESTION: Does that apply here?
+class serial_api(xcv_api):
+    """Serial API for handling a list of possible ports (class contains no data)"""
 
-        By avoiding controller-driver nonsense and just hacking into controllers and connecting the buttons to an arduino/teensy/whatever. 
-        On the arduino/teensy side of things, we then just parse out the commands and send some high/low signals to I/O pins
-        (other bits and bobs to handle all the I/O) and then a fancy display output to make things more fancy.
-    """
-
-    # ====================================
-    #   System
-    # ====================================
-    import sys
-
-    _WINDOWS = sys.platform.startswith("win")
+    BAUD = 115200
 
     @property
-    def WINDOWS(self) -> bool:
-        return self._WINDOWS
+    def port(self):
+        return self._port
 
-    # ====================================
-    #   Serial
-    # ====================================
-    SERIAL_BAUD: int = 115200
+    def port_next(self):
+        try:
+            self._port = self._ports.pop()
+        except IndexError:
+            pass
+        return self._port
 
-    @property
-    def SERIAL_PORT(self) -> str:
-        if self.WINDOWS:
-            return "COM17"
-        else:
-            return "/dev/cu.SLAB_USBtoUART"
+    def port_add(self, ser):
+        self._ports.append(ser)
 
-    # ====================================
-    #   CLI Styling
-    # ====================================
-    debug_indent: str = "\n\t\t"
-    centered_indent: str = "\n\t\t\t\t"
-    WARNING = f"{debug_indent}🥵  WARNING"
-    LAUNCHING = f"{debug_indent}🧨  LAUNCHING"
+    def port_set(self, ser):
+        self._port = ser
 
-    # ====================================
-    #   Paths
-    # ====================================
-    from pathlib import Path
+    def print_ports(self):
+        [print(_sp) for _sp in self._ports]
 
-    HOME_PATH: Path = Path.home()
-    ABSPATH: Path = Path(__file__).resolve().parent
+
+class os_is_win(serial_api):
+    def __init__(self):
+        """USB serial port settings for Windows"""
+        self._port = "COM17"
+        self._ports = ["COM18", "COM19", "COM20", "COM21"]
+
+
+class os_is_mac(serial_api):
+    def __init__(self):
+        """USB serial port settings for Mac"""
+        self._port = "/dev/cu.SLAB_USBtoUART"
+        self._ports = ["/dev/cu.usbmodem5821674", "/dev/cu.usbmodem5821675"]
+
+
+class os_is_other(serial_api):
+    def __init__(self):
+        """Empty USB serial port settings for Linux, FreeBSD, etc."""
+        self._port = input("Unknown OS - enter a USB Port: ")
+        self._ports = []
+
+
+if sys.platform.startswith("win"):
+    serial_config = os_is_win()
+    xcv_api.os_is_windows = True
+    emojis = {"boo": "Boo!", "robot": "", "stars": "Yay"}
+elif sys.platform.startswith("darwin"):
+    serial_config = os_is_mac()
+else:
+    serial_config = os_is_other()
 
 
 if __name__ == "__main__":
-    s = Settings()
-    print(s.SERIAL_PORT)
-    print(s.WINDOWS)
-    print(s.HOME_PATH)
+    from loguru import logger
+
+    logger.debug(f"Windows? {isinstance(os_config, os_is_win)}")
+
+    logger.debug(f"Serial Port: {os_config._port}")
+
+    os_config.port_next()
+
+    logger.debug(f"Serial Port: {os_config._port}")
+
+    print(os_config.emojis["stars"], os_config.emojis["boo"])
