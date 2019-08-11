630ea821354d9296740f17b21488987ba0087177
randy <randynewslettercatcher@gmail.com>
2019-08-11 01:00:57 -0400

    test imports

---
+++ tests/test_imports.py
@@ -0,0 +1,17 @@
+def test_imports():
+    try:
+        import cv2
+        import numpy
+        import loguru
+        import mypy
+        import black
+        import serial
+        import click
+        import PyQt5
+        import PySimpleGUIQt
+        import pytz
+        import streamlink
+    except:
+        raise
+
+    assert True
