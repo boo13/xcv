e8868a5d5a9714ba3950e79507212d27c68f98e4
randy <randynewslettercatcher@gmail.com>
2019-09-02 21:00:49 -0400

    no_microseconds added

--- xcv/clock.py
+++ xcv/clock.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from loguru import logger
 
@@ -14,6 +14,23 @@ class Clock:
     def elapsed(self):
         return self.now() - self._start
 
+    def elapsed_no_microseconds(self):
+        _e = self.elapsed()
+        return _e - timedelta(microseconds=_e.microseconds)
+
+    def elapsed_seconds(self):
+        _e = self.elapsed()
+        return int(_e.total_seconds())
+
     def reset(self):
         logger.debug("Clock Reset")
         self._start = self.now()
+
+
+if __name__ == "__main__":
+    from time import sleep
+
+    c = Clock()
+    sleep(2)
+    print(c.elapsed())
+    print(c.elapsed_no_microseconds())
