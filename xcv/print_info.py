e9683096e2285db27afe84f0e2689e66db9c9826
randy <randynewslettercatcher@gmail.com>
2019-09-03 04:29:20 -0400

    cleanup

--- xcv/print_info.py
+++ xcv/print_info.py
@@ -1,8 +1,7 @@
 import xcv
-from xcv.version import XCV_VERSION
 from xcv.emojis import BOO
 
 def print_package_info():
-    print(f"Author: {BOO}")
-    print(f"Email: {xcv.__email__}")
-    print(f"XCV Version: {XCV_VERSION}")
+    print(f"     {BOO}")
+    print(f"{xcv.__email__}")
+    print(f" XCV Version: {xcv.version.XCV_VERSION}")
